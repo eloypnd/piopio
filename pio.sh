1be53d4679f2e26755af4c45b76eebabebe2ae22
Eloy Pineda <me@eloy.codes>
2017-07-20 10:33:26 +0200

    remove `pio.sh`

--- pio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Simple bash script to run the proxy locally
-# without having to type the long pair key/secret all the time
-
-TWITTER_CONSUMER_KEY=your_twitter_app_consumer_key TWITTER_CONSUMER_SECRET=your_twitter_app_consumer_secret npm run dev
